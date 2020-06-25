d6469cc3be230098f9a66fef21b5c8362783b710
Lucas Correia <lucascorreia-.-@live.com>
2020-06-25 19:53:32 -0300

    feat: fetch data from google api

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'fakekey';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS'
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios.get<GoogleGeocodingResponse>(`https://maps.google.com/maps/fake/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    })
+}
+
+form.addEventListener('submit', searchAddressHandler);
